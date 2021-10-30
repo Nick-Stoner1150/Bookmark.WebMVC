d3f190e179957bfb61772040be9a1a381251ca91
Nicholas Stoner <nicholas.c.stoner@gmail.com>
2021-10-30 10:54:45 -0400

    added joining tabkle

---
+++ Bookmark.Data/BookAssignments.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmark.Data
+{
+    public class BookAssignments
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey(nameof(Book))]
+        public int BookId { get; set; }
+        public virtual Book Book { get; set; }
+
+        [ForeignKey(nameof(Bookshelf))]
+        public int BookshelfId { get; set; }
+        public virtual Bookshelf Bookshelf { get; set;  }
+    }
+}
---
+++ Bookmark.Data/Migrations/202110301402252_updatedBookshelf.cs
@@ -0,0 +1,16 @@
+﻿namespace Bookmark.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatedBookshelf : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
